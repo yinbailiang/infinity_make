b4c5608d95d49b9f18605e0c01f29b5234ba381c
yinbailiang <yinbailiang@foxmail.com>
2025-12-13 16:44:01 +0800

    modified:   .gitignore 	deleted:    infinity_build.ps1 	deleted:    infinity_dbg.ps1 	modified:   source/main.psm1

--- source/main.psm1
+++ source/main.psm1
@@ -2,8 +2,7 @@
 ##Import InfinityMake.Tools
 ##Import InfinityMake.Core
 function Invoke-Main([string[]]$ArgumentList) {
-    throw '你好！这是 Infinity Make 构建系统的主模块 (Main Module)。'
-    return 0
+    $Loger = [LogClient]::new($Script:DefaultLogServer)
 }
 
 Invoke-Main $args
